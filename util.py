fe5a39ef60674bb0de4253599cfde98fdb013c9c
zhenhao zhang <shaochen@zhenhaos-MacBook-Pro.local>
2021-09-06 15:33:53 -0400

    add plot

--- util.py
+++ util.py
@@ -109,7 +109,7 @@ def echo_attribute(inputs,neighs,threshold):
             grad_atts=np.hstack((grad_att1, grad_att2))
             grad_seq = (input_xfea * input_xfea.grad.data).cpu().detach().numpy().reshape(test_x_idx.shape[0], 61, 4,
                                                                                           1000)
-            process_data(pad_idx, test_x_idx.numpy(), grad_seq, grad_atts, chr)
+            process_data(pad_idx, test_x_idx.numpy(), np.abs(grad_seq), grad_atts, chr)
             tempo.append(torch.sigmoid(out).cpu().data.detach().numpy())
             if step % 1000 == 0:
                 print("step:", '%04d' % (step + 1), "time=", "{:.5f}".format(time.time() - t)
@@ -254,3 +254,163 @@ def generate_neighbors(input_sample_poi):
             temp_list.append(temp)
         neighs[chr] = np.array(temp_list)
     return neighs
+
+import matplotlib
+import matplotlib.pyplot as plt
+
+def plot_a(ax, base, left_edge, height, color):
+    a_polygon_coords = [
+        np.array([
+            [0.0, 0.0],
+            [0.5, 1.0],
+            [0.5, 0.8],
+            [0.2, 0.0],
+        ]),
+        np.array([
+            [1.0, 0.0],
+            [0.5, 1.0],
+            [0.5, 0.8],
+            [0.8, 0.0],
+        ]),
+        np.array([
+            [0.225, 0.45],
+            [0.775, 0.45],
+            [0.85, 0.3],
+            [0.15, 0.3],
+        ])
+    ]
+    for polygon_coords in a_polygon_coords:
+        ax.add_patch(matplotlib.patches.Polygon((np.array([1, height])[None, :] * polygon_coords
+                                                 + np.array([left_edge, base])[None, :]),
+                                                facecolor=color, edgecolor=color))
+
+
+def plot_c(ax, base, left_edge, height, color):
+    ax.add_patch(matplotlib.patches.Ellipse(xy=[left_edge + 0.65, base + 0.5 * height], width=1.3, height=height,
+                                            facecolor=color, edgecolor=color))
+    ax.add_patch(
+        matplotlib.patches.Ellipse(xy=[left_edge + 0.65, base + 0.5 * height], width=0.7 * 1.3, height=0.7 * height,
+                                   facecolor='white', edgecolor='white'))
+    ax.add_patch(matplotlib.patches.Rectangle(xy=[left_edge + 1, base], width=1.0, height=height,
+                                              facecolor='white', edgecolor='white', fill=True))
+
+
+def plot_g(ax, base, left_edge, height, color):
+    ax.add_patch(matplotlib.patches.Ellipse(xy=[left_edge + 0.65, base + 0.5 * height], width=1.3, height=height,
+                                            facecolor=color, edgecolor=color))
+    ax.add_patch(
+        matplotlib.patches.Ellipse(xy=[left_edge + 0.65, base + 0.5 * height], width=0.7 * 1.3, height=0.7 * height,
+                                   facecolor='white', edgecolor='white'))
+    ax.add_patch(matplotlib.patches.Rectangle(xy=[left_edge + 1, base], width=1.0, height=height,
+                                              facecolor='white', edgecolor='white', fill=True))
+    ax.add_patch(
+        matplotlib.patches.Rectangle(xy=[left_edge + 0.825, base + 0.085 * height], width=0.174, height=0.415 * height,
+                                     facecolor=color, edgecolor=color, fill=True))
+    ax.add_patch(
+        matplotlib.patches.Rectangle(xy=[left_edge + 0.625, base + 0.35 * height], width=0.374, height=0.15 * height,
+                                     facecolor=color, edgecolor=color, fill=True))
+
+
+def plot_t(ax, base, left_edge, height, color):
+    ax.add_patch(matplotlib.patches.Rectangle(xy=[left_edge + 0.4, base],
+                                              width=0.2, height=height, facecolor=color, edgecolor=color, fill=True))
+    ax.add_patch(matplotlib.patches.Rectangle(xy=[left_edge, base + 0.8 * height],
+                                              width=1.0, height=0.2 * height, facecolor=color, edgecolor=color,
+                                              fill=True))
+
+
+default_colors = {0: 'green', 1: 'blue', 2: 'orange', 3: 'red'}
+default_plot_funcs = {0: plot_a, 1: plot_c, 2: plot_g, 3: plot_t}
+
+
+def plot_weights_given_ax(tick,ax, array,
+                          figsize=(20, 2),
+                          height_padding_factor=0.2,
+                          length_padding=1.0,
+                          subticks_frequency=1.0,
+                          colors=default_colors,
+                          plot_funcs=default_plot_funcs,
+                          highlight={},
+                          ylabel=""):
+    if len(array.shape) == 3:
+        array = np.squeeze(array)
+    assert len(array.shape) == 2, array.shape
+    if (array.shape[0] == 4 and array.shape[1] != 4):
+        array = array.transpose(1, 0)
+    assert array.shape[1] == 4
+    max_pos_height = 0.0
+    min_neg_height = 0.0
+    heights_at_positions = []
+    depths_at_positions = []
+    for i in range(array.shape[0]):
+        # sort from smallest to highest magnitude
+        acgt_vals = sorted(enumerate(array[i, :]), key=lambda x: abs(x[1]))
+        positive_height_so_far = 0.0
+        negative_height_so_far = 0.0
+        for letter in acgt_vals:
+            plot_func = plot_funcs[letter[0]]
+            color = colors[letter[0]]
+            if (letter[1] > 0):
+                height_so_far = positive_height_so_far
+                positive_height_so_far += letter[1]
+            else:
+                height_so_far = negative_height_so_far
+                negative_height_so_far += letter[1]
+            plot_func(ax=ax, base=height_so_far, left_edge=i, height=letter[1], color=color)
+        max_pos_height = max(max_pos_height, positive_height_so_far)
+        min_neg_height = min(min_neg_height, negative_height_so_far)
+        heights_at_positions.append(positive_height_so_far)
+        depths_at_positions.append(negative_height_so_far)
+
+    # now highlight any desired positions; the key of
+    # the highlight dict should be the color
+    for color in highlight:
+        for start_pos, end_pos in highlight[color]:
+            assert start_pos >= 0.0 and end_pos <= array.shape[0]
+            min_depth = np.min(depths_at_positions[start_pos:end_pos])
+            max_height = np.max(heights_at_positions[start_pos:end_pos])
+            ax.add_patch(
+                matplotlib.patches.Rectangle(xy=[start_pos, min_depth],
+                                             width=end_pos - start_pos,
+                                             height=max_height - min_depth,
+                                             edgecolor=color, fill=False))
+
+    ax.set_xlim(-length_padding, array.shape[0] + length_padding)
+    ax.spines['right'].set_visible(False)
+    ax.spines['top'].set_visible(False)
+    ax.xaxis.set_ticks(np.arange(0.0, array.shape[0] + 1, 25))
+    ax.set_xticklabels(np.arange(tick-400, tick+600, 25), rotation=30)
+    ax.yaxis.set_ticks([])
+    ax.tick_params(axis='x', labelsize=30,length=30,width=3)
+    height_padding = max(abs(min_neg_height) * (height_padding_factor),
+                         abs(max_pos_height) * (height_padding_factor))
+    ax.set_ylim(min_neg_height - height_padding, max_pos_height + height_padding)
+    ax.spines['left'].set_visible(False)
+    ax.set_title('Attribution scores on the sequence '+str(tick)+'-'+
+                str(tick+200)+' bp with the flanking region ' +
+                '\naccording to TF binding on the central sequence')
+
+
+def plot_weights(array,tick,
+                 figsize=(150, 10),
+                 **kwargs):
+
+    fig = plt.figure(figsize=figsize)
+    ax = fig.add_subplot(111)
+    plot_weights_given_ax(tick,ax=ax, array=array, **kwargs)
+    plt.tight_layout()
+
+
+
+def plot_score_track_given_ax(arr, ax, threshold=None, **kwargs):
+    ax.plot(np.arange(len(arr)), arr, **kwargs)
+    if (threshold is not None):
+        ax.plot([0, len(arr) - 1], [threshold, threshold])
+    ax.set_xlim(0, len(arr) - 1)
+
+
+def plot_score_track(arr, threshold=None, figsize=(20, 2), **kwargs):
+    fig = plt.figure(figsize=figsize)
+    ax = fig.add_subplot(111)
+    plot_score_track_given_ax(arr, threshold=threshold, ax=ax, **kwargs)
+    plt.show()
